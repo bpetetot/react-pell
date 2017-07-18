0a98a7fa1879241872281323069d71611f428300
Benjamin Petetot <bpetetot@gmail.com>
2017-07-18 23:32:35 +0200

    :lipstick: pell custom css

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -5,27 +5,40 @@ import pell from 'pell'
 import 'pell/dist/pell.min.css'
 
 class Editor extends Component {
-
   componentDidMount() {
+    const { onChange, styleWithCSS, actionBarClass, buttonClass, contentClass } = this.props
     pell.init({
-      element: this.editor,
-      onChange: html => this.props.onChange(html),
-      styleWithCSS: this.props.styleWithCSS,
+      element: this.container,
+      onChange: html => onChange(html),
+      styleWithCSS,
+      classes: {
+        actionbar: actionBarClass,
+        button: buttonClass,
+        content: contentClass,
+      },
     })
   }
 
   render() {
-    return <div ref={e => (this.editor = e)} />
+    return <div ref={e => (this.container = e)} className={this.props.containerClass} />
   }
 }
 
 Editor.propTypes = {
   onChange: PropTypes.func.isRequired,
   styleWithCSS: PropTypes.bool,
+  containerClass: PropTypes.string,
+  actionBarClass: PropTypes.string,
+  buttonClass: PropTypes.string,
+  contentClass: PropTypes.string,
 }
 
 Editor.defaultProps = {
   styleWithCSS: false,
+  containerClass: 'pell-container',
+  actionBarClass: 'pell-actionbar',
+  buttonClass: 'pell-button',
+  contentClass: 'pell-content',
 }
 
 export default Editor
