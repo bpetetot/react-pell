544d7cb6372b57994f5def1b80c844161511d6b5
Benjamin Petetot <bpetetot@gmail.com>
2017-07-19 11:32:23 +0200

    add markdown to html for default content

--- src/plugins/markdown/withMarkdown.jsx
+++ src/plugins/markdown/withMarkdown.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
+import showdown from 'showdown'
 
-export default Editor => class extends Component {
+export default Editor =>
+  class extends Component {
+    handleMarkdownChange = (html) => {
+      this.props.onChange(html)
+    }
 
-  handleMarkdownChange = (html) => {
-    // here html to markdown
-    const markdown = html
-    console.log('yeah markdown !!!!')
-    this.props.onChange(markdown)
+    toHtml = (markdown) => {
+      const convertor = new showdown.Converter()
+      return convertor.makeHtml(markdown)
+    }
+
+    render() {
+      return (
+        <Editor
+          {...this.props}
+          defaultContent={this.toHtml(this.props.defaultContent)}
+          onChange={this.handleMarkdownChange}
+        />
+      )
+    }
   }
-
-  render() {
-    return <Editor {...this.props} onChange={this.handleMarkdownChange} />
-  }
-}
